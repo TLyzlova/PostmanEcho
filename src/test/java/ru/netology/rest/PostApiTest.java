de576c9dea5cbc2e371a63ced092d49a8131a615
Lyzlova Tatiana <yatanita@gmail.com>
2021-08-31 14:15:17 +0300

    Test failed

--- src/test/java/ru/netology/rest/PostApiTest.java
+++ src/test/java/ru/netology/rest/PostApiTest.java
@@ -10,18 +10,15 @@ class PostApiTest {
     @Test
     void shouldPostSomeData() {
 
-        // Given - When - Then
-// Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
+                .body("some data")
                 .when()
                 .post("/post")
-// Проверки
+
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo(" data"))
         ;
     }
 }
